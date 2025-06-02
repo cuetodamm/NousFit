193cacce30197ad57aa08eb84a70fffb50cda7d5
avilaYael <avica2330@outlook.com>
2025-06-01 21:21:50 -0600

    Modified existing files to improve app appearance and connectivity

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,10 @@
-import type { Metadata } from "next";
+'use client';
+
+
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { useEffect, useState } from "react";
+import { useRouter, usePathname } from "next/navigation";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -13,18 +17,30 @@ const geistMono = Geist_Mono({
 });
 
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }) {
+  const router = useRouter();
+  const pathname = usePathname();             //Detecta la página actual
+  const [authenticated, setAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token"); // Verifica si hay sesión activa
+    setAuthenticated(!!token);
+
+    if (!token && pathname === "/login") {
+      router.push("/login"); // Redirige al login si no está autenticado
+    }
+  }, [pathname]);
+
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+    <html lang="es">
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        {pathname === "/login" || authenticated ? (
+          <>
+            <main className="flex-1">{children}</main>
+          </>
+        ) : null}
       </body>
     </html>
   );
 }
+
